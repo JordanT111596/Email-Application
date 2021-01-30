3c1c638de55da8a91e6b2fb5da30f4908ef873d7
JordanT111596 <JordanT111596@gmail.com>
2021-01-30 16:39:47 -0500

    User can now input own name upon start up

--- EmailApplication/Email.java
+++ EmailApplication/Email.java
@@ -13,78 +13,92 @@ public class Email {
     private String alternateEmail;
     private String companySuffix = "jjindustries";
 
-    //Constructor to receive the first and last name, department, 
+    // Constructor to receive the first and last name, department,
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
         System.out.println("\n");
         System.out.println("Hello, " + this.firstName + " " + this.lastName + "!");
 
-        //Calls method asking for the department, then returns the response
+        // Calls method asking for the department, then returns the response
         this.department = setDepartment();
 
-        //Calls method that returns a random password
+        // Calls method that returns a random password
         this.password = randomPassword(defaultPasswordLength);
         System.out.println("Your password is: " + this.password);
 
-        //Combines user data to generate the email address
-        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix + ".com";
+        // Combines user data to generate the email address
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix
+                + ".com";
     }
 
-    //Asks for the department
+    // Asks for the department
     private String setDepartment() {
-        //Prints menu
-        System.out.println("Enter your department number!\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none");
-        //Initialize scanner
+        // Prints menu
+        System.out
+                .println("Enter your department number!\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none");
+        // Initialize scanner
         Scanner scanner = new Scanner(System.in);
-        //Next number entered will be department choice
+        // Next number entered will be department choice
         int depChoice = scanner.nextInt();
-        //If-Else corresponding with department numbers
-        if (depChoice == 1) { return "sales"; }
-        else if (depChoice == 2) { return "development"; }
-        else if (depChoice == 3) { return "accounting"; }
-        else { return ""; }
+        // If-Else corresponding with department numbers
+        if (depChoice == 1) {
+            return "sales";
+        } else if (depChoice == 2) {
+            return "development";
+        } else if (depChoice == 3) {
+            return "accounting";
+        } else {
+            return "";
+        }
     }
 
-    //Generates a random password
+    // Generates a random password
     private String randomPassword(int length) {
         String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*-+_";
         char[] password = new char[length];
-        //iterates through passwordSet to create a new password
+        // iterates through passwordSet to create a new password
         for (int i = 0; i < length; i++) {
-            //random assigns characters to password variable
+            // random assigns characters to password variable
             int rand = (int) (Math.random() * passwordSet.length());
             password[i] = passwordSet.charAt(rand);
         }
         return new String(password);
     }
 
-    //Sets the mailbox capacity with public API
+    // Sets the mailbox capacity with public API
     public void setMailboxCapacity(int capacity) {
         this.mailboxCapacity = capacity;
     }
 
-    //Sets the alternate email with public API
+    // Sets the alternate email with public API
     public void setAlternateEmail(String altEmail) {
         this.alternateEmail = altEmail;
     }
 
-    //Changes the password with public API
+    // Changes the password with public API
     public void changePassword(String password) {
         this.password = password;
     }
 
-    //Retrieves mailbox capacity with public API
-    public int getMailboxCapacity() { return mailboxCapacity; }
+    // Retrieves mailbox capacity with public API
+    public int getMailboxCapacity() {
+        return mailboxCapacity;
+    }
 
-    //Retrieves alternate email with public API
-    public String getAlternateEmail() { return alternateEmail; }
+    // Retrieves alternate email with public API
+    public String getAlternateEmail() {
+        return alternateEmail;
+    }
 
-    //Retrieves password with public API
-    public String getPassword() { return password; }
+    // Retrieves password with public API
+    public String getPassword() {
+        return password;
+    }
 
-    //Shows info requested (display name, company email, and mailbox capacity)
+    // Shows info requested (display name, company email, and mailbox capacity)
     public String showInfo() {
-        return "Your display name is: " + firstName + " " + lastName + "\n" + "Your company email is: " + email + "\n" + "Your mailbox capacity is: " + mailboxCapacity + "mb";
+        return "Your display name is: " + firstName + " " + lastName + "\n" + "Your company email is: " + email + "\n"
+                + "Your mailbox capacity is: " + mailboxCapacity + "mb";
     }
 }
--- EmailApplication/EmailApplication.java
+++ EmailApplication/EmailApplication.java
@@ -1,9 +1,42 @@
 package EmailApplication;
 
+import java.util.Scanner;
+
 public class EmailApplication {
     public static void main(String[] args) {
-        Email user1 = new Email("Demo", "User");
+
+        System.out.println("\n");
+
+        String firstName = setFirstName();
+
+        String lastName = setLastName();
+
+        Email user1 = new Email(firstName, lastName);
 
         System.out.println(user1.showInfo());
+    }
+
+    // Asks for first name from user
+    public static String setFirstName() {
+        // Asks user to enter a name
+        System.out.println("Please enter your first name");
+        // Initialize scanner
+        Scanner userScanner = new Scanner(System.in);
+        // Next string entered will be the first name
+        String fName = userScanner.next();
+        // returns the first name
+        return fName;
+    }
+
+    // Asks for last name from user
+    public static String setLastName() {
+        // Asks user to enter a name
+        System.out.println("Please enter your last name");
+        // Initialize scanner
+        Scanner userScanner = new Scanner(System.in);
+        // Next string entered will be the last name
+        String lName = userScanner.next();
+        // returns the last name
+        return lName;
     }
 }
